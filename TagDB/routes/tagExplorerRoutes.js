905b8ff7bdb2b0d3f7706e04fe93ba259666aa6f
Tamanna Zahir <128883456+Tamanna-Zahir@users.noreply.github.com>
2024-12-03 17:23:25 -0800

    Organize and streamline all the code

--- TagDB/routes/tagExplorerRoutes.js
+++ TagDB/routes/tagExplorerRoutes.js
@@ -5,33 +5,7 @@ const axios = require('axios');
 
 // Route to render the tag explorer page
 router.get("/", async (req, res) => {
-    // const sqlTags = 'SELECT * FROM tags'; // Query to get all tags
-    // db.query(sqlTags, (err, tagResults) => {
-    //     if (err) {
-    //         console.error(err);
-    //         return res.status(500).send("Database error");
-    //     }
 
-    //     // Query to get all tag values with corresponding tag ID
-    //     const sqlTagValues = 'SELECT * FROM tag_values';
-    //     db.query(sqlTagValues, (err, tagValueResults) => {
-    //         if (err) {
-    //             console.error(err);
-    //             return res.status(500).send("Database error");
-    //         }
-
-    //         // Combine tags and their values
-    //         const tagsWithValues = tagResults.map(tag => {
-    //             return {
-    //                 ...tag,
-    //                 tag_values: tagValueResults.filter(value => value.tag_id === tag.tag_id) // Match on tag_id
-    //             };
-    //         });
-
-    //         console.log(JSON.stringify(tagsWithValues, null, 2))          
-    //         res.render("tag-explorer", { tags: tagsWithValues });
-    //     });
-    // });
     try {
         const [tagsResponse, tagValuesResponse,defaultTagsResponse] = await Promise.all([
             axios.get('http://localhost:3000/tags'),
